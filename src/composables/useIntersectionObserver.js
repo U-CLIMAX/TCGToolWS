9290864127d22634d7254a038df0d0d99a3be3b3
Yuuzi261 <lokasu0531@gmail.com>
2025-11-11 16:28:23 +0800

    feat: add scroll reveal animations

---
+++ src/composables/useIntersectionObserver.js
@@ -0,0 +1,33 @@
+import { onUnmounted } from 'vue'
+
+export function useIntersectionObserver(target, onIntersect, options = { threshold: 0.1 }) {
+  let observer
+
+  const stop = () => {
+    if (observer) {
+      observer.disconnect()
+      observer = null
+    }
+  }
+
+  const start = () => {
+    stop() // Ensure any previous observer is disconnected
+
+    if (target.value) {
+      observer = new IntersectionObserver(([entry]) => {
+        if (entry.isIntersecting) {
+          onIntersect(entry.target)
+        }
+      }, options)
+
+      observer.observe(target.value)
+    }
+  }
+
+  onUnmounted(stop)
+
+  return {
+    start,
+    stop,
+  }
+}
