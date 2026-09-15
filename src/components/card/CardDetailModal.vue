ae83f912c4df57be98f40049c4c99de71c3e9310
FuseFairy <dd8611706@gmail.com>
2026-09-15 16:40:24 +0800

    fix(ui): disable text insertion caret globally on non-editable elements

--- src/components/card/CardDetailModal.vue
+++ src/components/card/CardDetailModal.vue
@@ -8,7 +8,7 @@
       right: handleSwipeRight,
     }"
     @click="handleModalClick"
-    class="d-flex flex-column w-100 cursor-auto"
+    class="d-flex flex-column w-100 cursor-default"
     style="position: relative"
     :class="{
       'overflow-y-auto themed-scrollbar': !$vuetify.display.mdAndUp,
